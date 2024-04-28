96351d2fbf7171f4ff6f706903aaf2a169dcd519
Pastito247 <117881893+Pastito247@users.noreply.github.com>
2024-04-28 15:16:49 -0400

    Login (sin diseño)

--- Kinebook/rate-repository-app/src/components/main.jsx
+++ Kinebook/rate-repository-app/src/components/main.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
 import Constants from 'expo-constants';
-import {  , Text, View } from 'react-native';
-
+import { View, Text, TextInput, Image, StyleSheet } from 'react-native';
 const Main = () => {
   return (
-    <View  >
-      <Text>Kinebook</Text>
-    </View> 
+    <View style={styles.container}>
+    <Image source={require('../img/image.png')} style={styles.image} />
+    <TextInput placeholder="Email" style={styles.input} />
+    <TextInput placeholder="Password" style={styles.input} secureTextEntry />
+    <View style={styles.buttonContainer}>
+      <Text style={styles.buttonText}>Login</Text>
+    </View>
+  </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    width: 300,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginVertical: 10,
+    paddingHorizontal: 10,
+  },
+  buttonContainer: {
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    width: 300,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
 export default Main
